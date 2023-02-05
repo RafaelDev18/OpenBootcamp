51710643acbe2114a7c02039c400ff7a79e6cb30
Rafael Rico <rafael.rico.018@gmail.com>
2023-02-05 15:16:21 -0500

    Creacion proyecto ejercicio tema 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Suma(10,20,30);
+    }
+    public static void Suma(int a, int b, int c){ // Funcion con retorno
+        int res = a + b + c;
+        System.out.println(res);
+    }
+
+    }
+}
